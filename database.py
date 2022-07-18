37feaac9f159962c749a31236f6d8e3cee24e7f2
MaureenOlum <104775402+MaureenOlum@users.noreply.github.com>
2022-07-18 18:35:38 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import sqlite3
+
+wikipedia = pd.read_excel("wikipedia_dataset_flat.xlsx", sheet_name='Sheet1', header=0, index_col=0)
+
+
+db_conn = sqlite3.connect("wikipedia.db")
+c = db_conn.cursor()
+
+c.execute(
+    """
+    CREATE TABLE wikipedia (
+        'date' TEXT,
+        'page' TEXT,
+        'visits' INTEGER
+        );
+    """
+)
+
+wikipedia.to_sql('wikipedia', db_conn, if_exists='append', index=False)
+
+pd.read_sql("SELECT * FROM wikipedia LIMIT 10", db_conn)
+
+db_conn.close()
